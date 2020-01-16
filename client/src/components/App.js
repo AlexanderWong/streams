b2ce591dbbc23ffdc81ee977b794bb687b54154b
Alexander Wong <35576253+AlexanderWong@users.noreply.github.com>
2020-01-16 14:51:29 -0800

    Streams folder added

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -22,8 +22,11 @@ function App() {
     <div>
       <BrowserRouter>
         <div>
-          <Route path="/" exact component={PageOne} />
-          <Route path="/pagetwo" exact component={PageTwo} />
+          <Route path="/" exact component={StreamList} />
+          <Route path="/streams/new" exact component={StreamCreate} />
+          <Route path="/streams/edit" exact component={StreamEdit} />
+          <Route path="/streams/delete" exact component={StreamDelete} />
+          <Route path="/streams/show" exact component={StreamShow} />
         </div>
       </BrowserRouter>
     </div>
