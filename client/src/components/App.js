bf995d5087ac08c3403069c7d6aeeea0e55204c5
Alexander Wong <35576253+AlexanderWong@users.noreply.github.com>
2020-01-16 14:35:15 -0800

    Initial commit

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,33 @@
 import React from "react";
+import { BrowserRouter, Route, Link } from "react-router-dom";
+
+const PageOne = () => {
+  return (
+    <div>
+      <Link to="/pagetwo">Go to page two</Link>
+    </div>
+  );
+};
+
+const PageTwo = () => {
+  return (
+    <div>
+      <Link to="/">Go to page one</Link>
+    </div>
+  );
+};
 
 function App() {
-  return <div>App</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <div>
+          <Route path="/" exact component={PageOne} />
+          <Route path="/pagetwo" exact component={PageTwo} />
+        </div>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
