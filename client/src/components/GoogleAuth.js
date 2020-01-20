cd0e41c77f4b4f362a3191218b6f151ddb397162
Alexander Wong <35576253+AlexanderWong@users.noreply.github.com>
2020-01-20 12:11:43 -0800

    sign in button

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -15,11 +15,45 @@ class GoogleAuth extends Component {
         .then(() => {
           this.auth = window.gapi.auth2.getAuthInstance();
           this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+          this.auth.isSignedIn.listen(this.onAuthChange);
         }); //this .then arrow function will execute when the gapi library is ready to go. get reference to auth object
     });
   }
+
+  onAuthChange = () => {
+    this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+  };
+
+  onSignInClick = () => {
+    this.auth.signIn();
+  };
+
+  onSignOutClick = () => {
+    this.auth.signOut();
+  };
+
+  renderAuthButton() {
+    if (this.state.isSignedIn === null) {
+      return null;
+    } else if (this.state.isSignedIn) {
+      return (
+        <button className="ui red google button" onClick={this.onSignOutClick}>
+          <i className="google icon"></i>
+          Sign Out
+        </button>
+      );
+    } else {
+      return (
+        <button className="ui green google button" onClick={this.onSignInClick}>
+          <i className="google icon"></i>
+          Sign in with Google
+        </button>
+      );
+    }
+  }
+
   render() {
-    return <div>GoogleAuth</div>;
+    return <div>{this.renderAuthButton()}</div>;
   }
 }
 
